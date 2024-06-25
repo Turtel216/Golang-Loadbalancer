80732d2aaf65de06bdbcb7d610d71e16be8913c1
Turtel216 <papadimitrios216@gmail.com>
2024-06-25 23:12:00 +0300

    Implemented weighted rr algorithm

--- weighted-round-robin/main.go
+++ weighted-round-robin/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -22,14 +24,14 @@ type simpleServer struct {
 }
 
 // Creates and returns a new instance of the simpleServer struct
-func newSimpleServer(addr string, weight int) *simpleServer {
+func newSimpleServer(addr string, weight int) simpleServer {
 	serverUrl, err := url.Parse(addr)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
 
-	return &simpleServer{
+	return simpleServer{
 		addr:   addr,
 		weight: weight,
 		proxy:  httputil.NewSingleHostReverseProxy(serverUrl),
@@ -39,19 +41,25 @@ func newSimpleServer(addr string, weight int) *simpleServer {
 type Loadbalancer struct {
 	port            string
 	roundRobinCount int
-	servers         []Server //Interface
+	servers         []simpleServer
+	current         int
 	total_weights   int
 }
 
 // Creates and returns a new loadbalancer instance
-func NewLoadBalancer(port string, servers []Server, total_weights int) *Loadbalancer {
-
-	return &Loadbalancer{
+func NewLoadBalancer(port string, servers []simpleServer, total_weights int) *Loadbalancer {
+	loadbalancer := &Loadbalancer{
 		port:            port,
 		roundRobinCount: 0,
 		servers:         servers,
 		total_weights:   total_weights,
 	}
+
+	for _, server := range servers {
+		loadbalancer.total_weights += server.weight
+	}
+
+	return loadbalancer
 }
 
 // Returns the adress of the simple server instance
@@ -66,21 +74,30 @@ func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
 }
 
 // returns the server selected by the weighted round-robin scheduler
-func (loadbalancer *Loadbalancer) getNextAvailableServer() (server Server) {
-	server = loadbalancer.servers[loadbalancer.roundRobinCount%len(loadbalancer.servers)]
-
-	for !server.IsAlive() {
-		loadbalancer.roundRobinCount++
-		server = loadbalancer.servers[loadbalancer.roundRobinCount%len(loadbalancer.servers)]
+func (loadbalancer *Loadbalancer) getNextAvailableServer() (simpleServer, error) {
+	if loadbalancer.current == -1 {
+		loadbalancer.current = rand.Intn(loadbalancer.total_weights)
 	}
 
-	loadbalancer.roundRobinCount++
-	return
+	for i := 0; i < loadbalancer.total_weights; i++ {
+		loadbalancer.current = (loadbalancer.current + 1) + loadbalancer.total_weights
+		loadbalancer.servers[loadbalancer.current].weight += loadbalancer.servers[loadbalancer.current].weight
+
+		if loadbalancer.servers[loadbalancer.current].weight >= loadbalancer.total_weights {
+			loadbalancer.servers[loadbalancer.current].weight -= loadbalancer.total_weights
+			return loadbalancer.servers[loadbalancer.current], nil
+		}
+	}
+
+	return simpleServer{}, errors.New("Couldn't find next avaible server")
 }
 
 // Forwards the request to the server returned by the getNextAvailableServer method
 func (loadbalancer *Loadbalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
-	target := loadbalancer.getNextAvailableServer()
+	target, err := loadbalancer.getNextAvailableServer()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("Forwarding request to adress %q\n", target.Address())
 
 	target.Serve(rw, req)
@@ -95,7 +112,7 @@ func main() {
 	flag.Parse()
 
 	// Target servers
-	servers := []Server{
+	servers := []simpleServer{
 		newSimpleServer("https://www.google.com", 3),
 		newSimpleServer("https://www.youtube.com", 2),
 		newSimpleServer("https://www.facebook.com", 1),
