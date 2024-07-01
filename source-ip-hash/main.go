d69afb60feaae5cf515d524ed8bcad08ee963e2e
Turtel216 <papadimitrios216@gmail.com>
2024-07-01 23:25:54 +0300

    Create ip hash loadbalancer

---
+++ source-ip-hash/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+)
+
+type Server interface {
+	Address() string
+	Serve(rw http.ResponseWriter, req *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+// Creates and returns a new instance of the simpleServer struct
+func newSimpleServer(addr string) simpleServer {
+	serverUrl, err := url.Parse(addr)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	return simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+type Loadbalancer struct {
+	port    string
+	mu      sync.Mutex
+	servers []simpleServer
+}
+
+// Creates and returns a new loadbalancer instance
+func NewLoadBalancer(port string, servers []simpleServer, total_weights int) *Loadbalancer {
+	loadbalancer := &Loadbalancer{
+		port:    port,
+		servers: servers,
+	}
+
+	return loadbalancer
+}
+
+// Returns the adress of the simple server instance
+func (s *simpleServer) Address() string { return s.addr }
+
+// Serves the through the reverse proxy
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+// returns the server selected by the weighted round-robin scheduler
+func (loadbalancer *Loadbalancer) getNextAvailableServer() (simpleServer, error) {
+	//TODO
+}
+
+// Forwards the request to the server returned by the getNextAvailableServer method
+func (loadbalancer *Loadbalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
+	target, err := loadbalancer.getNextAvailableServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Forwarding request to adress %q\n", target.Address())
+
+	target.Serve(rw, req)
+}
+
+func main() {
+
+	// Command line flag to specify port number when running go run ./src/main.go
+	// Default value is :4000
+	port := flag.String("port", "4000", "network port")
+
+	flag.Parse()
+
+	// Target servers
+	servers := []simpleServer{
+		newSimpleServer("https://www.google.com"),
+		newSimpleServer("https://www.youtube.com"),
+		newSimpleServer("https://www.facebook.com"),
+	}
+
+	// Creates a new loadbalancer at port 8000
+	loadbalancer := NewLoadBalancer(*port, servers, 6)
+
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		loadbalancer.serveProxy(rw, req)
+	}
+
+	//Retoutes request coming in at the `/` endpoint
+	http.HandleFunc("/", handleRedirect)
+
+	// Starting the server
+	fmt.Printf("Loadbalancer started at port %s \n", loadbalancer.port)
+	http.ListenAndServe(":"+loadbalancer.port, nil)
+}
