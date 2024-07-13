2ceb5bfd26540eb7f36f2ca16f8ae1b1f3de66f1
Turtel216 <papadimitrios216@gmail.com>
2024-07-13 22:23:42 +0300

    Added least response time algorithm to project

---
+++ least-response-time/least_response_time.go
@@ -0,0 +1,80 @@
+package least_response_time
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+)
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, req *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+
+	timer int64
+}
+
+// Creates and returns a new instance of the simpleServer struct
+func NewSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+
+		timer: 0,
+	}
+}
+
+type Loadbalancer struct {
+	Port string
+
+	mu      sync.Mutex
+	servers []Server //Interface
+}
+
+// Creates a new loadbalancer instance
+func NewLoadBalancer(port string, servers []Server) *Loadbalancer {
+	return &Loadbalancer{
+		Port:    port,
+		servers: servers,
+	}
+}
+
+// Returns the server selected by the round-robin scheduler
+func (loadbalancer *Loadbalancer) getNextAvailableServer() (server Server) {
+	loadbalancer.mu.Lock()
+	defer loadbalancer.mu.Unlock()
+	return
+}
+
+// Returns the adress of the simple server instance
+func (s *simpleServer) Address() string { return s.addr }
+
+// Ensures that the simpleServer instsance is running
+func (s *simpleServer) IsAlive() bool { return true }
+
+// Serves the through the reverse proxy
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+// Forwards the request to the server returned by the getNextAvailableServer method
+func (loadbalancer *Loadbalancer) ServeProxy(rw http.ResponseWriter, req *http.Request) {
+	target := loadbalancer.getNextAvailableServer()
+	fmt.Printf("Forwarding request to adress %q\n", target.Address())
+
+	target.Serve(rw, req)
+}
