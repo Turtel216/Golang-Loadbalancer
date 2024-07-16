dc2f355b0e75f727a4b2d4b71f9ba3aaeb3b96e5
Turtel216 <papadimitrios216@gmail.com>
2024-07-16 12:05:11 +0300

    Added config_parser function

---
+++ cmd/config_parser.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+)
+
+// parse the config file
+func parser_config(path string) ([]string, error) {
+	//read from config file
+	_bytes, err := read_config(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// convert byte slice to string
+	conf_str := string(_bytes)
+
+	//keeps track of the url of each iteratino
+	var url string
+	//stores all urls
+	var config []string
+
+	//iterate over string
+	for _, _char := range conf_str {
+		// add url to struct
+		if _char == '\n' {
+			config = append(config, url)
+			url = ""
+			continue
+		}
+
+		// append char to url
+		url += string(_char)
+	}
+
+	return config, nil
+}
+
+// Read from the config file
+func read_config(path string) ([]byte, error) {
+	//Read from the file
+	buff, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return read buffer
+	return buff, nil
+}
